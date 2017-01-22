8fb867007ac166106a595421eb9ab8df1f9c7157
hanangjie <hanangjie@gmail.com>
2017-01-22 16:09:47 +0800

    debu

--- js/data.js
+++ js/data.js
@@ -1,3 +1,15 @@
 var DATA={
-
+	game:[
+		{
+			name:"单机游戏",
+			list:[{
+				name:"贪吃蛇",
+				time:"2017-01-18",
+				detail:"吃豆豆看妹妹",
+				tag:["贪吃蛇","美图","2017"],
+				browse:504,
+				url:"../blog/Game/snake/index.html"
+			}]
+		}
+	]
 }
